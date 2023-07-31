0c25b6021a4390d2073273f9216c7ce7bb68e937
sathiya <113581441+narayanannn@users.noreply.github.com>
2023-07-31 09:45:44 +0530

    Add files via upload

---
+++ src/components/UI/slider/TestimonialSlider.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import Slider from "react-slick";
+import ava01 from "../../../assets/images/ava-1.jpg";
+import ava02 from "../../../assets/images/ava-2.jpg";
+import ava03 from "../../../assets/images/ava-3.jpg";
+import "../../../styles/slider.css";
+const TestimonialSlider = () => {
+  const settings = {
+    dots: true,
+    autoplay: true,
+    infinite: true,
+    speed: 1000,
+    autoplaySpeed: 3000,
+    swipeToSlide: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return (
+    <Slider {...settings}>
+      <div>
+        <p className="review__text">
+          "The company itself is a very successful company. Enduring
+          and how much less all the greater labors! It prevents him from being pursued
+          He hates something. He hates the very pain that you owe to our needs
+          what pain!"
+        </p>
+        <div className=" slider__content d-flex align-items-center gap-3 ">
+          <img src={ava01} alt="avatar" className=" rounded" />
+          <h6>Jhon Doe</h6>
+        </div>
+      </div>
+      <div>
+        <p className="review__text">
+          "The company itself is a very successful company. Enduring
+          and how much less all the greater labors! It prevents him from being pursued
+          He hates something. He hates the very pain that you owe to our needs
+          what pain!!"
+        </p>
+        <div className="slider__content d-flex align-items-center gap-3 ">
+          <img src={ava02} alt="avatar" className=" rounded" />
+          <h6>Mitchell Marsh</h6>
+        </div>
+      </div>
+      <div>
+        <p className="review__text">
+          "The company itself is a very successful company. Enduring
+          and how much less all the greater labors! It prevents him from being pursued
+          He hates something. He hates the very pain that you owe to our needs
+          what pain!!"
+        </p>
+        <div className="slider__content d-flex align-items-center gap-3 ">
+          <img src={ava03} alt="avatar" className=" rounded" />
+          <h6>Steven Crock</h6>
+        </div>
+      </div>
+    </Slider>
+  );
+};
+
+export default TestimonialSlider;
